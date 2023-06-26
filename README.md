7030d16bb87a9d182d4680a8bdf794ce261ae24b
Eggysgames <eggy55555@gmail.com>
2023-06-26 15:37:38 +0800

    Logic Changes
    Logic

--- README.md
+++ README.md
@@ -1,38 +1,11 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+=== Basic Chess === 
 
-## Getting Started
+This chessboard Github combines 2 popular chess repositories to make a workable chess engine with UI vs random moves as a test to get a working game operational in the fastest way possible to have a working template to use.
 
-First, run the development server:
+It uses React-Chessboard for the UI - https://www.npmjs.com/package/react-chessboard
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+It uses Chess.JS for the engine - https://github.com/jhlywa/chess.j
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+=== Custom Changes === 
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Added popup alerts for Checkmate and Draw
--- pages/chessgame.tsx
+++ pages/chessgame.tsx
@@ -1,28 +1,87 @@
-import { useEffect, useRef } from 'react';
-import Chessboard from 'chessboardjs';
-import { Chess } from 'chess.js';
+import { useState } from "react";
+import { Chess} from "chess.js";
+import { Chessboard } from "react-chessboard";
 
-function HomePage() {
+export default function PlayRandomMoveEngine() {
+  
+  const [game, setGame] = useState(new Chess());
 
-  useEffect(() => {
-    const chess = new Chess();
-  
-    const board = Chessboard('myBoard', 'start');
-  
-    // Use the chess instance as needed
-    console.log(chess.ascii());
+  let mover = false;
 
-    return () => {
-      
-    };
-  }, []);
+  function makeAMove(move: any) {
+
+    if (!game.isDraw()) {
+    try {
+      game.move(move);
+      if (mover == false) {
+        setTimeout(makeRandomMove, 200);
+        mover = true;
+      }
+    }
+    catch {
+      console.log("Invalid Move");
+    }
+
+    const newGame = new Chess(game.fen());
+    setGame(newGame);
+  }
+
+    if (game.isCheckmate()) {
+      setTimeout(() => {
+        window.alert("Checkmate");
+      }, 200);
+    }
+
+    if (game.isDraw()) {
+      setTimeout(() => {
+        window.alert("Draw");
+      }, 200);
+
+    }
   
+  }
+
+  function makeRandomMove(move:any) {
+    const possibleMoves = game.moves();
+    if (game.isCheckmate() || game.isDraw() || possibleMoves.length === 0) {
+      console.log(game.isCheckmate()); // returns false
+      console.log(game.isDraw()); // returns false
+      return; // exit if the game is over
+    }
+
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    makeAMove(possibleMoves[randomIndex]);
+  }
+
+  function onDrop(sourceSquare: string, targetSquare: string) {
+    const move = makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for simplicity
+    });
+
+    
+    // illegal move
+    if (move === null) {
+      return false;
+    }
+    
+    return true;
+    
+  }
+
   return (
-    <div>
-      <div id="myBoard" style={{ width: '400px' }}></div>
-      <div>CHESS</div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ width: '800px', height: '800px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ fontSize: '32px', fontWeight: 'bold', margin: 'auto' }}>Eggy&apos;s Chessboard</div>
+        <Chessboard
+          position={game.fen()}
+          onPieceDrop={onDrop}
+          areArrowsAllowed={true}
+        />
+      </div>
     </div>
   );
+  
+  
 }
-
-export default HomePage;
